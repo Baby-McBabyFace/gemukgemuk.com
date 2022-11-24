13bc53e9717634c5970f772340dfe38c936d0f93
Baby-McBabyFace <muttlt25@gmail.com>
2022-11-24 22:33:25 +0800

    EDit analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,12 @@ import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+    <script defer data-domain="gemukgemuk.com" src="http://192.168.1.209:8000/js/plausible.js"></script>
     <ThemeProvider defaultTheme="light" attribute="class">
       <Component {...pageProps} />
     </ThemeProvider>
+    </>
   );
 }
 
