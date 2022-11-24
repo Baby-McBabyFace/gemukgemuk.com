5ed406ffbc369b17233ac6ceb2003aff25b8ff95
Baby-McBabyFace <muttlt25@gmail.com>
2022-11-24 22:48:19 +0800

    edited analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,9 @@ import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-    <script defer data-domain="gemukgemuk.com" src="https://analytics.gemukgemuk.com/js/plausible.js"></script>
     <ThemeProvider defaultTheme="light" attribute="class">
       <Component {...pageProps} />
     </ThemeProvider>
-    </>
   );
 }
 
